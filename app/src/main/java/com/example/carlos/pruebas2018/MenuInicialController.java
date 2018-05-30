89c863691c529bf248c7e4b2a8b22c99dd0bc98a
Federico Aloi <federico.aloi@gmail.com>
2018-05-30 14:51:34 -0300

    Botón que agranda la letra
    (cherry picked from commit f9909f5d9b4503f4d316a91ac744420fd9f3c0c0)

--- app/src/main/java/com/example/carlos/pruebas2018/MenuInicialController.java
+++ app/src/main/java/com/example/carlos/pruebas2018/MenuInicialController.java
@@ -10,6 +10,12 @@ import org.threeten.bp.format.DateTimeFormatter;
 public class MenuInicialController extends BaseObservable {
     private String titulo = "Hola a todos";
     private String texto = "Estamos en el año " + Year.now().getValue();
+    private int tamanioLetra = 50;
+
+    @Bindable
+    public int getTamanioLetra() {
+        return tamanioLetra;
+    }
 
     @Bindable
     public String getTitulo() { return this.titulo; }
@@ -22,7 +28,16 @@ public class MenuInicialController extends BaseObservable {
         this.notifyPropertyChanged(BR.texto);
     }
 
+    public void setTamanioLetra(int tamanioLetra) {
+        this.tamanioLetra = tamanioLetra;
+        this.notifyPropertyChanged(BR.tamanioLetra);
+    }
+
     public void mostrarFechaDeHoy() {
         this.setTexto("Hoy es " + LocalDate.now().format(DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+    }
+
+    public void aumentarLetra() {
+        this.setTamanioLetra(this.tamanioLetra + 5);
     }
 }
