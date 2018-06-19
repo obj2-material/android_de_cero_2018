068103316211c2f26e96aa58f03cd96db30d08a2
Carlos Lombardi <carlombardi@gmail.com>
2018-06-19 03:28:19 -0300

    dejo más prolijo el controller de la lista de libros

--- app/src/main/java/com/example/carlos/pruebas2018/ListaDeLibrosController.java
+++ app/src/main/java/com/example/carlos/pruebas2018/ListaDeLibrosController.java
@@ -22,28 +22,42 @@ import java.util.List;
 public class ListaDeLibrosController extends BaseObservable {
 
     private AppCompatActivity activity;
+    // tener la lista de libros nos va a servir para responder al evento de clickear en uno,
+    // si se necesita obtener un libro a partir de la posicion
     private List<Libro> libros = new ArrayList<>();
-    private ArrayAdapter<Libro> librosAdapter;
+    // hay que recordar el adapter, para pasarle las filas a posteriori
+    private ArrayAdapter<Object> librosAdapter;
 
     public ListaDeLibrosController(AppCompatActivity _activity) {
         super();
         this.activity = _activity;
         this.librosAdapter = this.createLibrosAdapter();
+        // pongo una fila provisoria hasta que llegue la info del servidor
+        // una alternativa más elaborada es tener dos views, un cartel que diga
+        // "esperando información" y la lista, los dos en la misma posición,
+        // que se vea uno o el otro.
+        // en el llenarLibrosAdapter se esconde el cartel provisorio y se muestra la lista.
+        // para esto hay que saber cómo poner visible, o no, un view.
+        // imagino que para esto está el atributo android:visibility, aunque nunca lo usé.
+        this.librosAdapter.add("... obteniendo información ...");
         this.fetchLibros();
     }
 
     public ListAdapter getLibrosAdapter() { return this.librosAdapter; }
 
-    public ArrayAdapter<Libro> createLibrosAdapter() {
-        ArrayAdapter<Libro> adapter = new ArrayAdapter<>(
+    public ArrayAdapter<Object> createLibrosAdapter() {
+        ArrayAdapter<Object> adapter = new ArrayAdapter<>(
                 this.activity, android.R.layout.simple_list_item_1
         );
         return adapter;
     }
 
-    public void llenarLibrosAdapter() { this.librosAdapter.addAll(this.libros); }
+    public void llenarLibrosAdapter() {
+        this.librosAdapter.clear();
+        this.librosAdapter.addAll(this.libros);
+    }
 
-//    public List<XXXX> getLibros() {}
+//    public List<XXXX> getLibros() {}  -- esto no lo podemos hacer asi
     public void fetchLibros() {
 //        String url =  "https://restcountries.eu/rest/v2/region/americas";
         String url = "https://bibliography-card-app-zohafdyrxn.now.sh/books";
@@ -53,19 +67,8 @@ public class ListaDeLibrosController extends BaseObservable {
                 url,
                 null,
                 (JSONArray response) -> {
-                    try {
-                        for (int indice = 0; indice < response.length(); indice++) {
-                            JSONObject libro = response.getJSONObject(indice);;
-                            // ... hago cosas con el libro ...
-                            String titulo = libro.getString("title");
-                            int anio = libro.getInt("year");
-                            this.libros.add(new Libro(titulo, anio));
-                        }
-                        this.llenarLibrosAdapter();
-                    } catch (JSONException error) {
-                        error.printStackTrace();
-                        throw new RuntimeException("Error en el procesamiento del JSON", error);
-                    }
+                    this.procesarRespuestaDelServer(response);
+                    this.llenarLibrosAdapter();
                 },
                 (VolleyError error) -> {
                     error.printStackTrace();
@@ -75,5 +78,23 @@ public class ListaDeLibrosController extends BaseObservable {
 
         // OJO ver el onCreate de MenuInicial
         ApplicationToolset.toolset().addToRequestQueue(jsonArrayRequest);
+    }
+
+    /*
+     * Se separa el analisis del JSON en un metodo aparte
+     */
+    public void procesarRespuestaDelServer(JSONArray respuesta) {
+        try {
+            for (int indice = 0; indice < respuesta.length(); indice++) {
+                JSONObject libro = respuesta.getJSONObject(indice);;
+                // ... hago cosas con el libro ...
+                String titulo = libro.getString("title");
+                int anio = libro.getInt("year");
+                this.libros.add(new Libro(titulo, anio));
+            }
+        } catch (JSONException error) {
+            error.printStackTrace();
+            throw new RuntimeException("Error en el procesamiento del JSON", error);
+        }
     }
 }
